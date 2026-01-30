52c19e8003545d168395b968401ff7da70aeb040
kriangkrai <st651102064101@pcru.ac.th>
2026-01-30 11:51:53 +0700

    Update CLIENT_URL in config.js and remove peer flag from pg dependency in package-lock.json

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config(); // โหลดค่าจากไฟล์ .env เข้าสู่ process.env
 
 const config = {
-    CLIENT_URL: process.env.CLIENT_URL || 'http://project.3bbddns.com:5173'
+    CLIENT_URL: process.env.CLIENT_URL || 'https://ict.pcru.ac.th/~s651102064101/frontend/'
 };
 
 module.exports = config; 
